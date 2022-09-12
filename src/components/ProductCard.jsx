a119bc527974c9322c78c7c5962b56c432393c62
Benedicte Coulon <benedicte.coulon@wanadoo.fr>
2022-09-12 20:20:58 +0200

    axios done to display products list with product card component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "./components.css";
 
-function ProductCard() {
-  return <div>ProductCard</div>;
+function ProductCard({ product }) {
+  return (
+    <div key={product._id} className="product-card">
+      <h4>{product.product_name}</h4>
+      <img src={product.image_front_small_url} alt="product" />
+      <p>{product.id}</p>
+    </div>
+  );
 }
 
 export default ProductCard;
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import ProductCard from "./ProductCard";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./components.css";
 
 function ProductList() {
-  return <div>ProductList</div>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://world.openfoodfacts.org/cgi/search.pl?search_terms=nutella&search_simple=1&action=process&fields=id%2Cproduct_name%2Cimage_front_small_url&json=1&page=1&page_size=24`
+      )
+      .then((response) => {
+        console.log("response.data", response.data.products);
+        setProducts(response.data.products);
+      });
+  }, []);
+
+  return (
+    <div className="product-list">
+      {products.map((product) => (
+        <ProductCard product={product} />
+      ))}
+    </div>
+  );
 }
 
 export default ProductList;

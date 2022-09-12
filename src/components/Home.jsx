946e14d411cff26a346bca079802f54078730c94
Benedicte Coulon <benedicte.coulon@wanadoo.fr>
2022-09-12 19:36:39 +0200

    navbar created

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "./Navbar";
+import ProductList from "./ProductList";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home-component">
+      <Navbar />
+      <ProductList />
+    </div>
+  );
 }
 
 export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Logo from "../assets/logo-food-checking.png";
+import "./components.css";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <img className="logo" src={Logo} alt="" width="150px" />
+      <p>Menu</p>
+    </div>
+  );
 }
 
 export default Navbar;

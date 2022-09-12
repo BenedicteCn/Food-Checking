2ff9c3a64df59ad25143818386b22644d6b4eb8d
Benedicte Coulon <benedicte.coulon@wanadoo.fr>
2022-09-12 20:49:49 +0200

    search bar implemented

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -6,23 +6,53 @@ import "./components.css";
 
 function ProductList() {
   const [products, setProducts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     axios
       .get(
-        `https://world.openfoodfacts.org/cgi/search.pl?search_terms=nutella&search_simple=1&action=process&fields=id%2Cproduct_name%2Cimage_front_small_url&json=1&page=1&page_size=24`
+        `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${query}&search_simple=1&action=process&fields=id%2Cproduct_name%2Cimage_front_small_url&json=1&page=1&page_size=24`
       )
       .then((response) => {
         console.log("response.data", response.data.products);
         setProducts(response.data.products);
+      })
+      .catch((err) => {
+        console.error(err);
       });
-  }, []);
+  }, [query]);
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(searchTerm);
+  };
 
   return (
-    <div className="product-list">
-      {products.map((product) => (
-        <ProductCard product={product} />
-      ))}
+    <div classname="app">
+      <h2>Recherche un produit dans notre base de données :</h2>
+      <div className="search-bar">
+        <form className="search-form" onSubmit={getSearch}>
+          <input
+            className="search-input"
+            type="text"
+            placeholder="Rechercher un produit"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          ></input>
+          <button className="search-button" type="submit">
+            Chercher
+          </button>
+        </form>
+      </div>
+
+      <h3>Résultats de la recherche</h3>
+
+      <div className="product-list">
+        {products.map((product) => (
+          <ProductCard product={product} />
+        ))}
+      </div>
     </div>
   );
 }

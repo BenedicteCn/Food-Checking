4d80ed136373c45303275829c7e1c48bcfc9cded
Bénédicte Coulon <benedictecoulon@airdebenedicte.home>
2022-09-13 18:18:39 +0200

    Implementation of one page per product with its details (component ProductDetails

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { useState, useEffect } from "react"; // <== IMPORT useEffect
+import { useParams } from "react-router-dom";
+import axios from "axios"; // <== IMPORT axios
 
 function ProductDetails() {
-  return <div>ProductDetails</div>;
+  const params = useParams();
+  console.log(params.id);
+
+  const [oneProduct, setOneProduct] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://world.openfoodfacts.org/api/v0/product/${params.id}.json?fields=product_name%2Ccategories%2Cimage_front_url%2Callergens_hierarchy%2Cingredients_text`
+      )
+      .then((response) => {
+        console.log("response.data", response.data.product);
+        setOneProduct(response.data.product);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [params.id]);
+
+  return (
+    <div className="product-details">
+      <h3>{oneProduct.product_name}</h3>
+      <img
+        src={oneProduct.image_front_url}
+        alt={oneProduct.product_name}
+      />
+      <p><h4>Catégories :</h4> {oneProduct.categories}</p>
+      <p><h4>Allergènes :</h4> {oneProduct.allergens_hierarchy}</p>
+      <p><h4>Ingrédients : </h4>
+        <ul>
+        {oneProduct.ingredients_text}
+        </ul>
+      </p>
+    </div>
+  );
 }
 
 export default ProductDetails;

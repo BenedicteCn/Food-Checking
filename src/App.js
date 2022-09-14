3a8ef127a57723df8289331f549639343f3f7ea4
Bénédicte Coulon <benedictecoulon@airdebenedicte.home>
2022-09-14 13:35:23 +0200

    Footer component added in App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import ProductDetails from "./components/ProductDetails";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/:id" element={<ProductDetails />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );

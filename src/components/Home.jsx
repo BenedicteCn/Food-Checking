9d9ef0bda4f7915a83b35557e57680424ae5cd15
Bénédicte Coulon <benedictecoulon@airdebenedicte.home>
2022-09-14 14:20:55 +0200

    Creation of pagination component (separated from productlist component)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import Pagination from "./Pagination";
 import ProductList from "./ProductList";
+import { useState } from "react";
+
 
 function Home() {
+  const [products, setProducts] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0)
+
   return (
     <div className="home-component">
-      <ProductList />
+      <ProductList products={products} setProducts={setProducts} pageNumber={pageNumber} setPageNumber={setPageNumber}/>
+      <Pagination setPageNumber={setPageNumber} products={products}/>
     </div>
   );
 }
---
+++ src/components/Pagination.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./components.css";
+import ReactPaginate from "react-paginate";
+
+
+function Pagination( { setPageNumber, products  }) {
+  const productsPerPage = 8
+  const pageCount = Math.ceil(products.length / productsPerPage)
+  const changePage = ({selected}) => {
+    setPageNumber(selected)
+  } 
+
+  return (
+      <div className="pagination">
+      <ReactPaginate 
+          previousLabel={"Précédent"}
+          nextLabel={"Suivant"}
+          pageCount={pageCount}
+          onPageChange={changePage}
+        />
+      </div>
+  );
+}
+
+export default Pagination;

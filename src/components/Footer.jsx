351895cea68ddd77313e681e472ed9baaeb1b973
Bénédicte Coulon <benedictecoulon@airdebenedicte.home>
2022-09-14 13:34:10 +0200

    Footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./components.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+        <p>@ Food Checking 2022</p>
+        </div>
+  )
+}
+
+export default Footer

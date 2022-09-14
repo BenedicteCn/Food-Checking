b823e4d7d8e3a865c44dc4b658db7bec736e03bd
Bénédicte Coulon <benedictecoulon@airdebenedicte.home>
2022-09-14 14:24:48 +0200

    menu logo added on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import Logo from "../assets/logo-food-checking.png";
 import { Link } from "react-router-dom";
 import "./components.css";
+import Menu from "../assets/menu.png";
 
 function Navbar() {
   return (
     <div className="navbar">
       <Link to="/">
-        <img className="logo" src={Logo} alt="" width="150px" />
+        <img className="navarbar-logo" src={Logo} alt="" />
       </Link>
-      <p>Menu</p>
+      <img className="navbar-menu" src={Menu} alt=""/>
+
     </div>
   );
 }

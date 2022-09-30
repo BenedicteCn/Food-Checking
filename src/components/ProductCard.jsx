10e1750941809280d63c4a2f4df02f6f5e5835dd
Bénédicte Coulon <benedictecoulon@airdebenedicte.home>
2022-09-30 11:57:54 +0200

    bug fixed for links

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./ProductCard.css";
 
 function ProductCard({ product }) {
   return (
     <div key={product._id} className="product-card">
-      <a href={`/${product.id}`}>
+      <Link to={`/${product.id}`}>
         <h4>{product.product_name}</h4>
         <img src={product.image_front_small_url} className="product-card-image" alt={product.product_name}  />
-      </a>
+      </Link>
     </div>
   );
 }
